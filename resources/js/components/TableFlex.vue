fac73fda8f3ac3588a380fa1bc08681c679993d5
David_Trendmatch <david@trendmatch.dk>
2019-09-17 10:03:36 +0200

    up to date

---
+++ resources/js/components/TableFlex.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex-table">
+            <div class="header-row flex-table-row">
+
+            </div>
+            <slot class="product-row flex-table-row">
+
+            </slot>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'tableFlex'
+}
+</script>
+
+<style>
+
+</style>
