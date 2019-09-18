47990894010d53ac36e5adc826e76802ec855cde
David_Trendmatch <david@trendmatch.dk>
2019-09-18 09:54:41 +0200

    up to date

--- public/js/app.js
+++ public/js/app.js
@@ -8587,6 +8587,10 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
     submitDisabled: function submitDisabled() {
       if (this.newComment.comment.length < 1 || this.submittingComment || this.currentTeamId < 0) return true;else return false;
     },
+    placeholderText: function placeholderText() {
+      var filter = this.commentFilter;
+      if (filter == 'remarks') return 'Write a new remark..';else return 'Write a comment..';
+    },
     commentsFiltered: function commentsFiltered() {
       var _this = this;
 
@@ -17829,7 +17833,7 @@ var render = function() {
             attrs: {
               name: "comment",
               id: "comment-input",
-              placeholder: "Write a comment..",
+              placeholder: _vm.placeholderText,
               oninput:
                 'this.style.height = "";this.style.height = this.scrollHeight + "px"'
             },
