451b66e1f0aff7d78c6f914f6aea69c6d840aa62
David_Trendmatch <david@trendmatch.dk>
2019-11-29 12:07:09 +0100

    attempt_01 to add custom build scripts on deploy

--- resources/js/components/screens/Collection.vue
+++ resources/js/components/screens/Collection.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="collection">
-        <h1>Files</h1>
+        <h1>Files test</h1>
         <div class="underline"></div>
         <div class="filters">
             <!-- <Dropdown class="dropdown-parent left">
