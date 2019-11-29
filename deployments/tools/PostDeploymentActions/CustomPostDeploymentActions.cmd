41fed961b43ddd8943d5167a50ecc756e10403e7
David_Trendmatch <david@trendmatch.dk>
2019-11-29 21:14:32 +0100

    attempt_19 add custom build scripts on deploy

--- deployments/tools/PostDeploymentActions/CustomPostDeploymentActions.cmd
+++ deployments/tools/PostDeploymentActions/CustomPostDeploymentActions.cmd
@@ -1,7 +1,7 @@
 :: 1. Post Deployment Custom scripts
 echo Running custom post deployment scripts
-echo generate key
-call php artisan key:generate 
+:: echo generate key
+:: call php artisan key:generate 
 echo Resetting the configuration cache
 call php artisan config:cache
 echo route cache
