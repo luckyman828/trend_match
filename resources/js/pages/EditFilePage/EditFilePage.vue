803e778307140bea04df74a8121936bca785befc
David_Trendmatch <david@trendmatch.dk>
2020-03-24 15:12:29 +0100

    Added recommended action to edit products page: Shift variants so variants with images are shown first

--- resources/js/pages/EditFilePage/EditFilePage.vue
+++ resources/js/pages/EditFilePage/EditFilePage.vue
@@ -3,6 +3,14 @@
 
         <ThePageHeader :title="`Editing: ${file.name}`"/>
 
+        <div class="quick-actions" v-if="productsEligibleForVariantImageShift.length > 0">
+            <p>Recommended actions</p>
+            <button v-if="productsEligibleForVariantImageShift.length > 0" class="ghost md dark" 
+            @click="setVariantWithImageFirst" style="margin-right: 8px;">
+                <span>Update {{productsEligibleForVariantImageShift.length}} products: Show variants with image first</span>
+            </button>
+        </div>
+
         <ProductsTable :sortKey="sortKey"
         :file="file" :products="productsFiltered"
         @onSort="onSort"/>
@@ -40,13 +48,35 @@ export default{
         file() {
             return this.currentFile
         },
+        productsEligibleForVariantImageShift() {
+            return this.products.filter(x => {
+                if (x.variants.length < 2) return false
+                const noImageIndex = x.variants.findIndex(x => !x.image)
+                return noImageIndex >= 0 && x.variants.slice(noImageIndex+1).find(x => !!x.image)
+            })
+        }
     },
     methods: {
+        ...mapActions('products', ['updateProduct']),
         ...mapMutations('products', ['setSingleVisisble']),
         onSort(method, key) {
             this.sortKey = key
             this.sortArray(this.products, method, key)
         },
+        setVariantWithImageFirst() {
+            const productsToUpdateCount = this.productsEligibleForVariantImageShift.length
+            // Loop through all products, move the variants with images first and save them
+            this.products.forEach(product => {
+                if (product.variants.length > 1) {
+                    product.variants.sort((a,b) => {
+                        if (!!a.image && !b.image) return -1
+                        if (!!b.image && !a.image) return 1
+                    })
+                    this.updateProduct(product)
+                }
+            })
+            alert(`Success! ${productsToUpdateCount} Products updated`)
+        }
     },
     created() {
         // Initially sort the products
@@ -56,5 +86,20 @@ export default{
 </script>
 
 <style scoped lang="scss">
-    @import '~@/_variables.scss';
+@import '~@/_variables.scss';
+    .quick-actions {
+        border-bottom: solid 2px $light2;
+        padding-bottom: 16px;
+        margin-bottom: 16px;
+        p {
+            font-size: 14px;
+            font-weight: 500;
+            margin-bottom: 8px;
+        }
+        .button {
+            &:not(:last-child) {
+                margin-right: 12px;
+            }
+        }
+    }
 </style>
