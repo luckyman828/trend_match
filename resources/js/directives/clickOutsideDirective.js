6f2a7c2e2e78d18d734241697216afc56169d66f
David_Trendmatch <david@trendmatch.dk>
2020-05-05 10:03:41 +0200

    Changed the way the click-outside directive works. Now both the click origin and click end have to be outside of the element the click-outside directive is attached to before the event fires. This fixes the unwanted behaviour where context menus with search would close when selecting the entire search query  and accidentally ending the click outside the context menu

--- resources/js/directives/clickOutsideDirective.js
+++ resources/js/directives/clickOutsideDirective.js
@@ -2,20 +2,32 @@ const clickOutsideDirective = {}
 clickOutsideDirective.install = Vue => {
     Vue.directive('click-outside', {
         bind: function(el, binding, vnode) {
+            let clickOriginTarget = null
             el.clickOutsideEvent = function(event) {
                 // here I check that click was outside the el and his childrens
                 if (
-                    !(el == event.target || el.contains(event.target) || !document.contains(event.target))
-                    // || event.target.closest('.context-menu')
+                    !(el == event.target || el.contains(event.target) || !document.contains(event.target)) &&
+                    !(
+                        el == clickOriginTarget ||
+                        el.contains(clickOriginTarget) ||
+                        !document.contains(clickOriginTarget)
+                    )
                 ) {
                     // and if it did, call method provided in attribute value
                     vnode.context[binding.expression](event)
                 }
             }
-            document.body.addEventListener('click', el.clickOutsideEvent)
+            // HANDLE MOUSEDOWN
+            // On mousedown save a reference to the element where the click originated
+            el.clickStartEvent = function(event) {
+                clickOriginTarget = event.target
+            }
+            document.body.addEventListener('mouseup', el.clickOutsideEvent)
+            document.body.addEventListener('mousedown', el.clickStartEvent)
         },
         unbind: function(el) {
-            document.body.removeEventListener('click', el.clickOutsideEvent)
+            document.body.removeEventListener('mouseup', el.clickOutsideEvent)
+            document.body.removeEventListener('mousedown', el.clickStartEvent)
         },
     })
 }
