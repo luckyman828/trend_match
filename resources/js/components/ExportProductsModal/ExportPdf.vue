89fe66961c5fa1f71d99c0e12d361f6c471c0b6c
David_Trendmatch <david@trendmatch.dk>
2020-05-27 12:08:50 +0200

    Moved PDF generation to another library to fix a range of bugs surrounding the old library. Most notably we can not display most/some emojis and we are exporting way way smaller PDFs

--- resources/js/components/ExportProductsModal/ExportPdf.vue
+++ resources/js/components/ExportProductsModal/ExportPdf.vue
@@ -3,24 +3,39 @@
         <div class="overlay" @click="$emit('close')"></div>
         <div class="wrapper" ref="pdfWrapper">
             <div class="inner" style="font-family: 'Roboto', sans-serif, helvetica, arial; position: relative;">
-                <div class="page" v-for="(productChunk, index) in productChunks" :key="index"
-                style="width: 576pt; height: 792pt; box-sizing: border-box; padding: 60px 38px 38px;
-                display: flex; flex-wrap: wrap; justify-content: space-between; align-items: space-between;">
-                    <div class="product-wrapper" v-for="product in productChunk" :key="product.datasource_id"
-                    style="border: solid 2px; border-radius: 4px; height: calc(100% / 3 - 12px); box-sizing: border-box;
-                    padding: 8px 12px;"
-                    :style="{width: exportComments ? '100%' : 'calc(50% - 12px)'}">
-                        <div class="col-wrapper" style="display: flex; justify-content: space-between; height: 100%;">
-                            <div class="col-left" style="display: flex; flex-direction: column;
-                            justify-content: space-between; align-items: space-between;"
-                            :style="{width: exportComments ? 'calc(25% - 6px)' : 'calc(33% - 6px)'}">
+
+
+                <div class="page" v-for="(productChunk, index) in chunksToShow" :key="index"
+                style="width: 744pt; height: 1052.5pt; box-sizing: border-box; padding: 60px 38px 38px; position: relative;">
+
+                    <div class="header" style="display: -webkit-box; -webkit-box-pack: justify; justify-content: space-between;
+                    position: absolute; top: 16px; left 0; width: 684pt;">
+                        <div class="left">
+                            <p style="margin: 0; font-weight: 700">{{currentFile.name}}</p>
+                            <p style="margin: 0; margin-top: 8px; font-weight: 400">{{formatDate(new Date)}}</p>
+                        </div>
+                        <div class="right" style="text-align: right;">
+                            <p style="margin: 0; font-weight: 400">Page {{index+1}} of {{totalPages}}</p>
+                        </div>
+                    </div>
+
+                    <div class="product-wrapper" v-for="(product, index) in productChunk" :key="product.datasource_id"
+                    style="display: block; border: solid 2px; border-radius: 4px; height: 306pt; box-sizing: border-box;
+                    padding: 8px 12px; margin-top: 12pt"
+                    :style="[{width: exportComments ? '100%' : '336pt'},
+                    {marginRight: !exportComments && index%2 == 0 ? '12pt' : '0'},
+                    {float: !exportComments && index%2 == 0 ? 'left' : 'right'}]">
+                        <div class="col-wrapper" style="display: -webkit-box; -webkit-box-pack: justify; justify-content: space-between; height: 100%;">
+                            <div class="col-left" style="display: -webkit-box; -webkit-box-orient: vertical; flex-direction: column;
+                            -webkit-box-pack: justify; justify-content: space-between; align-items: space-between;"
+                            :style="{width: exportComments ? '24%' : '32%'}">
                                 <div class="row-top">
                                     <table class="id">
                                         <tr>
                                             <td>{{product.datasource_id}}</td>
                                         </tr>
                                         <tr>
-                                            <td style="font-weight: 700;">{{product.title}}</td>
+                                            <td style="font-weight: 700;"><h4 style="margin: 0;">{{product.title}}</h4></td>
                                         </tr>
                                         <tr>
                                             <td>{{product.delivery_date}}</td>
@@ -28,11 +43,13 @@
                                     </table>
                                 </div>
                                 <div class="row-bottom">
-                                    <div class="img-sizer" style="width: 92px;">
+                                    <div class="img-sizer" :style="{width: exportComments ? '160px' : '132px'}">
                                         <div class="img-wrapper" style="position: relative; padding-top: 133.3333%;
-                                        border: solid 2px; height: 0; width: 100%;">
-                                            <img :src="variantImage(product.variants[0])" style="height: 100%; width: 100%; position: absolute;
-                                            left: 0; top: 0; object-fit: contain;">
+                                        border: solid 2px; height: 0; width: 100%; margin-bottom: 8px;
+                                        background-size: contain; background-position: center; background-repeat: no-repeat;"
+                                        :style="{backgroundImage: `url(${variantImage(product.variants[0])})`}">
+                                            <!-- <img :src="variantImage(product.variants[0])" style="height: 100%; width: 100%; position: absolute;
+                                            left: 0; top: 0; object-fit: contain;"> -->
                                         </div>
                                     </div>
                                     <table class="prices">
@@ -52,50 +69,49 @@
                                 </div>
                             </div>
 
-                            <div class="col-mid" style="display: flex; flex-direction: column;
-                            justify-content: space-between; align-items: space-between;"
-                            :style="{width: exportComments ? 'calc(25% - 6px)' : 'calc(33% - 6px)'}">
+                            <div class="col-mid" style="display: -webkit-box; -webkit-box-orient: vertical; flex-direction: column;
+                            -webkit-box-pack: justify; justify-content: space-between; align-items: space-between;"
+                            :style="{width: exportComments ? '24%' : '32%'}">
                                 <div class="row">
-                                    <div class="assortments" style="display: flex; flex-direction: column; width: 100%;">
+                                    <div class="assortments" style="display: -webkit-box; -webkit-box-orient: vertical; flex-direction: column; width: 100%;">
                                         <div v-for="(assortment, index) in product.assortments" :key="index"
                                         style="line-height: 1; word-break: break-word; margin-bottom: 2px;">
-                                            <span style="font-size: 7px;">{{assortment.name || 'Unknown assortment'}} ({{assortment.box_size || 0}})</span>
+                                            <span style="font-size: 10px;">{{assortment.name || 'Unknown assortment'}} ({{assortment.box_size || 0}})</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="col-right" style="display: flex; flex-direction: column;
-                            justify-content: space-between; align-items: space-between;"
-                            :style="{width: exportComments ? 'calc(50% - 6px)' : 'calc(33% - 6px)'}">
+                            <div class="col-right" style=""
+                            :style="{width: exportComments ? '49%' : '32%'}">
                                 <div class="input" v-if="includeDistribution || exportComments">
                                     <!-- Alignment -->
-                                    <div class="alignment-wrapper" style="margin-bottom: 12px; border-bottom: solid 1px #E4E4E4;">
-                                        <strong style="font-size: 8px;">Alignment & Requests</strong>
+                                    <div class="alignment-wrapper" style="margin-bottom: 20px; border-bottom: solid 1px #E4E4E4;">
+                                        <strong style="font-size: 11px; margin-bottom: 4px; display: block;">Alignment & Requests</strong>
                                         <div class="row" v-for="(action, index) 
                                         in product.actions.filter(
                                             x => includeNotDecided ? true : (x.action != 'None' 
                                             || product.requests.find(request => request.selection_id == x.selection_id)))" 
                                         :key="'alignment-'+index"
-                                        style="display: flex; flex-direction: row; width: 100%; align-items: center; border-top: solid 1px #E4E4E4;">
-                                            <div style="overflow: hidden; white-space: nowrap; max-width: 60px; min-width: 60px;">
-                                                <td style="font-size: 7px;">
-                                                    <span style="font-size: 7px;">{{action.selection.name}}</span>
+                                        style="display: -webkit-box; flex-direction: row; width: 100%; align-items: center; border-top: solid 1px #E4E4E4;">
+                                            <div style="overflow: hidden; white-space: nowrap; max-width: 80px; min-width: 80px;">
+                                                <td style="font-size: 10px;">
+                                                    <span style="font-size: 10px;">{{action.selection.name | truncate(16)}}</span>
                                                 </td>
                                             </div>
                                             <div v-if="includeDistribution" 
-                                            style="font-size: 7px; max-width: 24px; min-width: 24px; margin-left: 8px;" 
+                                            style="font-size: 10px; max-width: 28px; min-width: 28px; margin-left: 16px;" 
                                             :style="{textAlign: product.actions.find(x => x.selection_id == action.selection_id).action == 'Out' ? 'right' : 'left'}">
-                                                <span>
-                                                    {{product.actions.find(x => x.selection_id == action.selection_id).action == 'Out' ? 'O' 
-                                                    : product.actions.find(x => x.selection_id == action.selection_id).action == 'Focus' ? 'F' 
-                                                    : product.actions.find(x => x.selection_id == action.selection_id).action == 'In' ? 'I' : ''}}
+                                                <span style="font-size: 12px; margin-bottom: -3px; display: block; margin-top: -2px; font-weight: 900;">
+                                                    {{product.actions.find(x => x.selection_id == action.selection_id).action == 'Out' ? '⨯' 
+                                                    : product.actions.find(x => x.selection_id == action.selection_id).action == 'Focus' ? '★' 
+                                                    : product.actions.find(x => x.selection_id == action.selection_id).action == 'In' ? '♥' : ''}}
                                                 </span>
                                             </div>
                                             <!-- Requests -->
                                             <div v-if="exportComments" 
-                                            style="font-size: 7px; flex: 1; margin-left: 12px;">
-                                                <span style="font-size: 7px; display: block;" 
+                                            style="font-size: 10px; -webkit-box-flex: 1; margin-left: 16px; padding: 1px;">
+                                                <span style="font-size: 10px; display: block; margin-bottom: 4px;" 
                                                 v-for="(request, index) in product.requests.filter(x => x.selection_id == action.selection_id)" 
                                                 :key="'request-'+index">
                                                     {{request.content}}
@@ -107,33 +123,30 @@
 
                                     <!-- Feedback -->
                                     <div class="feedback-wrapper" style="border-bottom: solid 1px #E4E4E4;">
-                                        <strong style="font-size: 8px;">Feedback & Comments</strong>
-                                        <div class="row" v-for="(user, index) 
-                                        in currentSelection.users.filter(user => includeNotDecided ? true
-                                        : (product.feedbacks.find(x => x.user_id == user.id) && product.feedbacks.find(x => x.user_id == user.id).action != 'None')
-                                        || product.comments.filter(x => x.user_id == user.id).length > 0)" 
+                                        <strong style="font-size: 11px; margin-bottom: 4px; display: block;">Feedback & Comments</strong>
+                                         <div class="row" v-for="(feedback, index) 
+                                        in product.feedbacks.filter(feedback => includeNotDecided ? true
+                                        : feedback.action != 'None')" 
                                         :key="index"
-                                        style="display: flex; flex-direction: row; width: 100%; align-items: center; border-top: solid 1px #E4E4E4;">
+                                        style="display: -webkit-box; flex-direction: row; width: 100%; align-items: center; border-top: solid 1px #E4E4E4;">
                                             <div style="overflow: hidden; white-space: nowrap; max-width: 60px; min-width: 60px;">
-                                                <td style="font-size: 7px;">
-                                                    <span style="font-size: 7px;">{{user.name || 'Unknown user'}}</span>
+                                                <td style="font-size: 10px;">
+                                                    <span style="font-size: 10px;">{{feedback.user ? feedback.user.name : 'Anonymous' | truncate(16)}}</span>
                                                 </td>
                                             </div>
                                             <div v-if="includeDistribution" 
-                                            style="font-size: 7px; max-width: 24px; min-width: 24px; margin-left: 8px;" 
-                                            :style="{textAlign: !!product.feedbacks.find(x => x.user_id == user.id) 
-                                            && product.feedbacks.find(x => x.user_id == user.id).action == 'Out' ? 'right' : 'left'}">
-                                                <span v-if="!!product.feedbacks.find(x => x.user_id == user.id) 
-                                                && product.feedbacks.find(x => x.user_id == user.id).action != 'None'"
-                                                style="font-size: 7px;">
-                                                    {{product.feedbacks.find(x => x.user_id == user.id).action == 'Out' ? 'O' 
-                                                    : product.feedbacks.find(x => x.user_id == user.id).action == 'Focus' ? 'F' : 'I'}}
+                                            style="font-size: 10px; max-width: 28px; min-width: 28px; margin-left: 16px;" 
+                                            :style="{textAlign: feedback.action == 'Out' ? 'right' : 'left'}">
+                                                <span v-if="feedback.action != 'None'"
+                                                style="font-size: 12px; margin-bottom: -3px; display: block; margin-top: -2px; font-weight: 900; width: 100%;">
+                                                    {{feedback.action == 'Out' ? '⨯' 
+                                                    : feedback.action == 'Focus' ? '★' : '♥'}}
                                                 </span>
                                             </div>
                                             <div v-if="exportComments" 
-                                            style="font-size: 7px; flex: 1; margin-left: 12px;">
-                                                <span style="font-size: 7px; display: block;" 
-                                                v-for="(comments, index) in product.comments.filter(x => x.user_id == user.id)" 
+                                            style="font-size: 10px; flex: 1; margin-left: 16px; padding: 1px;">
+                                                <span style="font-size: 10px; display: block; margin-bottom: 4px;" 
+                                                v-for="(comments, index) in product.comments.filter(x => x.user_id == feedback.user_id)" 
                                                 :key="'comment-'+index">
                                                     {{comments.content}}
                                                 </span>
@@ -167,6 +180,7 @@ export default {
         'exportComments',
         'includeDistribution',
         'includeNotDecided',
+        'chunkIndex'
     ],
     computed: {
         ...mapGetters('workspaces', ['currentWorkspace']),
@@ -186,6 +200,17 @@ export default {
             }
             return chunkedArr;
         },
+        chunksToShow() {
+            return this.productChunks
+            // const size = this.exportComments ? 3 : 6
+            // const target = 12 // MUST be divisible by 6
+            // const chunkAmount = target / size
+            // const start = this.chunkIndex * chunkAmount
+            // return this.productChunks.slice(start, start + chunkAmount)
+        },
+        totalPages() {
+            return this.chunksToShow.length
+        }
     }
 }
 </script>
