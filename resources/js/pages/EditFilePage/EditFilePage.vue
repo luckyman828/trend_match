36ceb084113a142d13b9c9d69cfa03092646b1de
David_Trendmatch <david@trendmatch.dk>
2020-11-09 09:59:39 +0100

    Fix shift variants with images first code updating all products with multiple variants, and sending multiple api requests. Now only sending 1 api request that updates all the products affected

--- resources/js/pages/EditFilePage/EditFilePage.vue
+++ resources/js/pages/EditFilePage/EditFilePage.vue
@@ -1,13 +1,9 @@
 <template>
     <div class="edit-file-component">
-
         <!-- <ThePageHeader :title="`Editing: ${file.name}`"/> -->
-        <h1>Editing: {{file.name}}</h1>
+        <h1>Editing: {{ file.name }}</h1>
 
-        <button class="primary md"
-            style="margin-bottom: 20px"
-            @click="showBulkUpload = !showBulkUpload"
-        >
+        <button class="primary md" style="margin-bottom: 20px" @click="showBulkUpload = !showBulkUpload">
             <template v-if="!showBulkUpload">
                 <i class="far fa-upload"></i>
                 <span>Bulk upload images</span>
@@ -18,22 +14,26 @@
             </template>
         </button>
 
-        <BulkUploadComponent v-if="showBulkUpload"/>
+        <BulkUploadComponent v-if="showBulkUpload" />
 
         <div class="quick-actions" v-if="productsEligibleForVariantImageShift.length > 0">
             <p>Recommended actions</p>
-            <button v-if="productsEligibleForVariantImageShift.length > 0" class="ghost md dark" 
-            @click="setVariantWithImageFirst" style="margin-right: 8px;">
-                <span>Update {{productsEligibleForVariantImageShift.length}} products: Show variants with image first</span>
+            <button
+                v-if="productsEligibleForVariantImageShift.length > 0"
+                class="ghost md dark"
+                @click="setVariantWithImageFirst"
+                style="margin-right: 8px;"
+            >
+                <span
+                    >Update {{ productsEligibleForVariantImageShift.length }} products: Show variants with image
+                    first</span
+                >
             </button>
         </div>
 
-        <ProductsTable :sortKey="sortKey"
-        :file="file" :products="productsFiltered"
-        @onSort="onSort"/>
+        <ProductsTable :sortKey="sortKey" :file="file" :products="productsFiltered" @onSort="onSort" />
 
-        <ProductFlyin :show="singleVisible"
-        @closeSingle="setSingleVisisble(false)" @onSort="onSort"/>
+        <ProductFlyin :show="singleVisible" @closeSingle="setSingleVisisble(false)" @onSort="onSort" />
     </div>
 </template>
 
@@ -47,7 +47,7 @@ import ThePageHeader from '../../components/layout/ThePageHeader'
 // Mixins
 import sortArray from '../../mixins/sortArray'
 
-export default{
+export default {
     name: 'editFilePage',
     components: {
         ProductsTable,
@@ -55,13 +55,13 @@ export default{
         ThePageHeader,
         BulkUploadComponent,
     },
-    mixins: [
-        sortArray
-    ],
-    data: function () { return {
-        sortKey: 'sequence',
-        showBulkUpload: false,
-    }},
+    mixins: [sortArray],
+    data: function() {
+        return {
+            sortKey: 'sequence',
+            showBulkUpload: false,
+        }
+    },
     computed: {
         ...mapGetters('files', ['currentFile']),
         ...mapGetters('products', ['products', 'productsFiltered', 'singleVisible']),
@@ -69,41 +69,41 @@ export default{
             return this.currentFile
         },
         productsEligibleForVariantImageShift() {
-            return this.products.filter(x => {
-                if (x.variants.length < 2) return false
-                const noImageIndex = x.variants.findIndex(x => !x.image)
-                return noImageIndex >= 0 && x.variants.slice(noImageIndex+1).find(x => !!x.image)
-            })
-        }
+            return this.products.filter(x => this.checkProductEligibleForVariantShift(x))
+        },
     },
     methods: {
-        ...mapActions('products', ['updateProduct']),
+        ...mapActions('products', ['updateManyProducts']),
         ...mapMutations('products', ['setSingleVisisble', 'SET_LAST_SORT']),
         ...mapMutations('alerts', ['SHOW_SNACKBAR']),
         onSort(method, key) {
             this.sortKey = key
             this.sortArray(this.products, method, key)
             // Save a reference to our last sort
-            this.SET_LAST_SORT({method, key})
+            this.SET_LAST_SORT({ method, key })
         },
         setVariantWithImageFirst() {
-            const productsToUpdateCount = this.productsEligibleForVariantImageShift.length
             // Loop through all products, move the variants with images first and save them
-            this.products.forEach(product => {
-                if (product.variants.length > 1) {
-                    product.variants.sort((a,b) => {
-                        if (!!a.image && !b.image) return -1
-                        if (!!b.image && !a.image) return 1
-                    })
-                    this.updateProduct(product)
-                }
+            const productsToUpdate = []
+            this.productsEligibleForVariantImageShift.map(product => {
+                productsToUpdate.push(product)
+                product.variants.sort((a, b) => {
+                    if (!!a.pictures.find(x => !!x.url) && !b.pictures.find(x => !!x.url)) return -1
+                    if (!!b.pictures.find(x => !!x.url) && !a.pictures.find(x => !!x.url)) return 1
+                })
             })
-            this.SHOW_SNACKBAR({
-                msg: `${productsToUpdateCount} Product${productsToUpdateCount > 1 ? 's' : ''} updated`,
-                iconClass: 'fa-check',
-                type: 'success'
-            })
-        }
+            this.updateManyProducts({ file: this.currentFile, products: productsToUpdate })
+        },
+        checkProductEligibleForVariantShift(product) {
+            // Exclude products with no addtional variants to shift around
+            if (product.variants.length < 2) return false
+            // Check if the product has a variant with no image bu the next variants do have an image
+            const noImageIndex = product.variants.findIndex(x => !x.pictures.find(picture => !!picture.url))
+            if (noImageIndex < 0) return false
+            // Return true if there is a variant after the found one, that does have an image
+            const nextVariants = product.variants.slice(noImageIndex + 1)
+            return !!nextVariants.find(x => x.pictures.find(picture => !!picture.url))
+        },
     },
     created() {
         // Initially sort the products
@@ -114,19 +114,19 @@ export default{
 
 <style scoped lang="scss">
 @import '~@/_variables.scss';
-    .quick-actions {
-        border-bottom: $borderDivider;
-        padding-bottom: 16px;
-        margin-bottom: 16px;
-        p {
-            font-size: 14px;
-            font-weight: 500;
-            margin-bottom: 8px;
-        }
-        .button {
-            &:not(:last-child) {
-                margin-right: 12px;
-            }
+.quick-actions {
+    border-bottom: $borderDivider;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+    p {
+        font-size: 14px;
+        font-weight: 500;
+        margin-bottom: 8px;
+    }
+    .button {
+        &:not(:last-child) {
+            margin-right: 12px;
         }
     }
+}
 </style>
