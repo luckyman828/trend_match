d320952f4bf27d8559232c7f4449fb6270d29091
David_Trendmatch <david@trendmatch.dk>
2021-01-06 14:53:47 +0100

    Change object fit for prewview to fit image2

--- resources/js/pages/WatchVideoPresentationPage/ProductDetailsSidebar/ProductPreview.vue
+++ resources/js/pages/WatchVideoPresentationPage/ProductDetailsSidebar/ProductPreview.vue
@@ -136,7 +136,7 @@ export default {
         img {
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            object-fit: contain;
             display: block;
             position: absolute;
             left: 0;
