79ca73c9fb2e107ef666e938f3457dd175ab34e6
David_Trendmatch <david@trendmatch.dk>
2021-01-21 11:25:03 +0100

    Overhaul button icon spacing

--- resources/js/components/common/SelectionIcon.vue
+++ resources/js/components/common/SelectionIcon.vue
@@ -1,9 +1,9 @@
 <template>
     <i v-if="selection.type == 'Master'" class="fa-poll master" :class="selection.id ? 'fas' : 'far'"
-        ><i class="fas fa-crown"></i
+        ><i class="fas fa-crown pos-bottom pos-left primary"></i
     ></i>
     <i v-else-if="selection.type == 'Chapter'" class="fa-project-diagram master" :class="selection.id ? 'fas' : 'far'">
-        <i class="fas fa-filter"></i>
+        <i class="fas fa-filter pos-bottom pos-left primary"></i>
     </i>
     <i class="fa fa-poll" v-else></i>
 </template>
@@ -20,20 +20,20 @@ export default {
 i {
     width: 24px;
     font-size: 16px;
-    &.master {
-        position: relative;
-        i {
-            position: absolute;
-            left: -3px;
-            bottom: 5px;
-            font-size: 11px;
-            color: $primary;
-            margin: 0;
-            width: auto;
-        }
-        &::after {
-            opacity: 1;
-        }
-    }
+    // &.master {
+    //     position: relative;
+    //     i {
+    //         position: absolute;
+    //         left: -3px;
+    //         bottom: 5px;
+    //         font-size: 11px;
+    //         color: $primary;
+    //         margin: 0;
+    //         width: auto;
+    //     }
+    //     &::after {
+    //         opacity: 1;
+    //     }
+    // }
 }
 </style>
