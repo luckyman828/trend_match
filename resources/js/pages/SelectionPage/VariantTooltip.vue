764500f1c04ec73dd8025ed8eefa63bdb3b2633c
David_Trendmatch <david@trendmatch.dk>
2021-02-15 09:53:39 +0100

    Fix product being set to ND when changing variant feedback from FOCUS to IN

--- resources/js/pages/SelectionPage/VariantTooltip.vue
+++ resources/js/pages/SelectionPage/VariantTooltip.vue
@@ -163,7 +163,7 @@ export default {
                 this.newQuantity = 0
             }
             let currentAction
-            let newProductAction
+            let newProductAction = newAction
 
             if (this.selectionMode == 'Feedback') {
                 // Find the users feedback action for the product and make sure it is not None
