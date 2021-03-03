0b27bd3230d23ea12d2be53d0c488a73f55376c2
Long Le <levanlongktmt@gmail.com>
2021-03-04 05:35:22 +0700

    fix load url of resized image

--- resources/js/mixins/variantImage.js
+++ resources/js/mixins/variantImage.js
@@ -14,10 +14,25 @@ export default {
             }
 
             if (size) {
-                if (size == 'sm') urlToReturn = `${urlToReturn}-300x300`
-                if (size == 'lg') urlToReturn = `${urlToReturn}-1024x1024`
+                if (size == 'sm') urlToReturn = this.getResizedImageUrl(urlToReturn, '300x300')
+                if (size == 'lg') urlToReturn = this.getResizedImageUrl(urlToReturn, '1024x1024')
             }
             return urlToReturn
         },
+        getResizedImageUrl(url, size) {
+            const components = url.split('/')
+            let fileName = components[components.length - 1]
+            components.splice(components.length - 1, 1)
+            if (fileName.includes('.')) {
+                const dotIdx = fileName.lastIndexOf('.')
+                const name = `${fileName.substring(0, dotIdx)}-${size}`;
+                fileName = name + fileName.substring(dotIdx)
+            }
+            else {
+                fileName = `${fileName}-${size}`
+            }
+            components.push(fileName)
+            return components.join('/')
+        }
     },
 }
