ea1c33bc703cda018e067307cd02c783f984a8c8
David-44Pixel <david@44pixel.com>
2021-08-06 09:45:09 +0200

    Add visual indicator to timing item preview, to show if it is in wishlist and or basket

--- resources/js/pages/PLAYB2C/WatchPresentationPage/Desktop/PreviewListItem.vue
+++ resources/js/pages/PLAYB2C/WatchPresentationPage/Desktop/PreviewListItem.vue
@@ -1,7 +1,7 @@
 <template>
     <v-popover trigger="hover" placement="right" popoverClass="min pad-xs" ref="popover">
         <div
-            class="variant-preview flex-list flex-v min"
+            class="preview-list-item flex-list flex-v min"
             v-if="variant"
             @click="SET_PDP_ITEM({ product: variant.product, variant })"
         >
@@ -17,6 +17,16 @@
                     }}
                     {{ variant.product.yourPrice.currency }}</span
                 >
+            </div>
+            <div class="icon-list flex-list space-sm">
+                <div class="circle xs red" v-if="isInWishlist">
+                    <i class="fas fa-heart"></i>
+                </div>
+                <button class="circle xs primary" v-if="isInBasket">
+                    <i class="far fa-shopping-bag">
+                        <i class="fas fa-check pos-right pos-bottom white"></i>
+                    </i>
+                </button>
             </div>
         </div>
         <div class="action-list flex-list space-xs" slot="popover">
@@ -36,7 +46,7 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 import AddToWishlistButton from '../AddToWishlistButton.vue'
 import AddToBasketButton from '../AddToBasketButton'
 
@@ -44,6 +54,22 @@ export default {
     name: 'PreviewListItem',
     components: { AddToWishlistButton, AddToBasketButton },
     props: ['variant'],
+    computed: {
+        ...mapGetters('basket', {
+            getItemIsInBasket: 'getItemIsInBasket',
+        }),
+        ...mapGetters('wishlist', {
+            getVariantIsInWishlist: 'getVariantIsInWishlist',
+        }),
+        isInWishlist() {
+            return this.getVariantIsInWishlist(this.variant)
+        },
+        isInBasket() {
+            return this.getItemIsInBasket({
+                variant: this.variant,
+            })
+        },
+    },
     methods: {
         ...mapMutations('playPresentation', ['SET_PDP_ITEM']),
         hidePopover() {
@@ -56,13 +82,14 @@ export default {
 <style lang="scss" scoped>
 @import '~@/_variables.scss';
 
-.variant-preview {
+.preview-list-item {
     width: 100%;
     background: white;
     border-radius: 2px;
     overflow: hidden;
     cursor: pointer;
     border-radius: 8px;
+    position: relative;
     .img-wrapper {
         width: 100%;
     }
@@ -72,6 +99,11 @@ export default {
     .price {
         font-size: 12px;
         font-weight: 500;
+    }
+    .icon-list {
+        top: 4px;
+        right: 4px;
+        position: absolute;
     }
 }
 </style>
