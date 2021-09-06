922a43b5ce3f4218fa5e288b9bce8ffd83ef94e0
David-44Pixel <david@44pixel.com>
2021-09-06 18:20:25 +0200

    Fix it again

--- resources/js/store/modules/integrations/dkc/bonaparte.js
+++ resources/js/store/modules/integrations/dkc/bonaparte.js
@@ -69,11 +69,11 @@ export default {
         async fetchProductsBySearch({ dispatch, rootGetters }, searchString) {
             const enabledFeatures = rootGetters['workspaces/getEnabledFeatures']
             // let baseUrl = null
+            const locale = 'DA_DK'
+            const theLocale = locale ? locale.toLowerCase().replaceAll('_', '-') : 'da-dk'
             // TEMP
             let baseUrl = `https://search.bonaparteshop.com/api/${theLocale}/v1.0/product/search`
             // END TEMP
-            const locale = 'DA_DK'
-            const theLocale = locale ? locale.toLowerCase().replaceAll('_', '-') : 'da-dk'
             if (enabledFeatures.play_shop_bap_qa) {
                 baseUrl = `https://search-bap-qa.bap-test.com/api/${theLocale}/v1.0/product/search`
             }
@@ -200,11 +200,11 @@ export default {
         },
         async fetchProduct({ rootGetters }, { product, locale }) {
             const enabledFeatures = rootGetters['workspaces/getEnabledFeatures']
+            const theLocale = locale ? locale.toLowerCase().replaceAll('_', '-') : 'da-dk'
             // let baseUrl = null
             // TEMP
             let baseUrl = `https://search.bonaparteshop.com/api/${theLocale}/v2.0/Product/GetStyle`
             // END TEMP
-            const theLocale = locale ? locale.toLowerCase().replaceAll('_', '-') : 'da-dk'
             if (enabledFeatures.play_shop_bap_qa) {
                 baseUrl = `https://search-bap-qa.bap-test.com/api/${theLocale}/v2.0/Product/GetStyle`
                 // ?style=30306309&includeVariantNo=194023`
